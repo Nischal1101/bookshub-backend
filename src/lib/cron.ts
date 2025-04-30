833e4162193c0cf663d0c009600e27c7e10d6e92
nischal1101 <kaflenk123@gmail.com>
2025-04-30 08:56:39 +0545

    fix issue in protectRoute

--- src/lib/cron.ts
+++ src/lib/cron.ts
@@ -2,14 +2,13 @@ import { CronJob } from "cron";
 import https from "https";
 import { API_URL } from "../config";
 
-const job=new CronJob("*/14 * * * *", () => {
-  https.get(API_URL!,(res)=>{
-    if(res.statusCode===200){
+const job = new CronJob("*/14 * * * *", () => {
+  https.get(API_URL!, (res) => {
+    if (res.statusCode === 200) {
       console.log("Cron job is running");
-    }
-    else{
+    } else {
       console.log("Cron job is not running");
     }
-  })
-    })
+  });
+});
 export default job;
--- src/middlewares/protectRoute.ts
+++ src/middlewares/protectRoute.ts
@@ -1,18 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
+import User from "../models/User";
 
-const protectRoute = (req: Request, res: Response, next: NextFunction) => {
+const protectRoute = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
   try {
     const decoded = jwt.verify(token, JWT_SECRET as string);
-    (req as any).user = decoded;
+    const user = await User.findById((decoded as any).userId).select(
+      "-password"
+    );
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    (req as any).user = user;
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: "Unauthorized, token not vaild" });
   }
 };
 export default protectRoute;
