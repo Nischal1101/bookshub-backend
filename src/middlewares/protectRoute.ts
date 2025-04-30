763a902ca108a67dfd280d5e147cb2720449f760
nischal1101 <kaflenk123@gmail.com>
2025-04-30 17:19:25 +0545

    add debugging for token

--- src/middlewares/protectRoute.ts
+++ src/middlewares/protectRoute.ts
@@ -9,6 +9,7 @@ const protectRoute = async (
   next: NextFunction
 ) => {
   const token = req.headers.authorization?.split(" ")[1];
+  console.log("token", token);
   if (!token) {
     return res
       .status(401)
